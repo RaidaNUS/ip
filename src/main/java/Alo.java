74a2f0484611b66dd4194da09dd8d3fecaaf27b2
RaidaNUS <adrita.raida@gmail.com>
2025-01-31 14:58:13 +0800

    Level-1 : Echo Implementation

--- src/main/java/Alo.java
+++ src/main/java/Alo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Alo {
     public static void main(String[] args) {
 
@@ -6,10 +8,23 @@ public class Alo {
         System.out.println("How may I be of assistance to you today?");
         System.out.println("____________________________________________________________");
 
-        // Simulating user typing "bye"
-        System.out.println("____________________________________________________________");
-        System.out.println("Alrighty! Nice helping you. Bye!"); // Exit message
-        System.out.println("____________________________________________________________");
+        Scanner scan = new Scanner(System.in);
 
+        while (true) {
+            String input = scan.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Great Helping You! Ciao ^_^");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                //ECHOING
+                System.out.println("____________________________________________________________");
+                System.out.println("       " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        scan.close();
     }
 }
