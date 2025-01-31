225facac7f4bb3e17b6dd901b532fe42ea2c33c1
RaidaNUS <adrita.raida@gmail.com>
2025-01-31 15:14:27 +0800

    Level-2: List Implementation

--- src/main/java/Alo.java
+++ src/main/java/Alo.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Alo {
     public static void main(String[] args) {
@@ -9,21 +10,31 @@ public class Alo {
         System.out.println("____________________________________________________________");
 
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
         while (true) {
-            String input = scan.nextLine();
+            String input = scan.nextLine().trim();
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Great Helping You! Ciao ^_^");
                 System.out.println("____________________________________________________________");
                 break;
-            } else {
-                //ECHOING
-                System.out.println("____________________________________________________________");
-                System.out.println("       " + input);
-                System.out.println("____________________________________________________________");
             }
+
+            if(input.equalsIgnoreCase("list")){
+                System.out.println("____________________________________________________________");
+                for(int i= 0; i<tasks.size();i+=1){
+                    System.out.println("      " + (i+1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+
+            tasks.add(input);
+            System.out.println("____________________________________________________________");
+            System.out.println("         added: "+ input);
+            System.out.println("____________________________________________________________");
         }
         scan.close();
     }
