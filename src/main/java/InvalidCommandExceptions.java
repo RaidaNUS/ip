ec7ff4f41d6dbb4947929b7fce8f9ec1baa3622b
RaidaNUS <adrita.raida@gmail.com>
2025-02-14 13:37:17 +0800

    Level 5: Using Exception Class to Make TaskExceptions Class
    (cherry picked from commit a2ff3f6985b3845375736847b1e446a53918f68c)

---
+++ src/main/java/InvalidCommandExceptions.java
@@ -0,0 +1,5 @@
+public class InvalidCommandExceptions extends TaskExceptions {
+    public InvalidCommandExceptions(){
+        super("Invalid or No Command Word. Try using deadline, event, todo as initiators for tasks! ^_^");
+    }
+}
